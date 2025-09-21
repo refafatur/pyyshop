bdacb049cde1584caa5c53dbbc913c4645f24978
Muhamad Refa Fatur Rahman <150717285+refafatur@users.noreply.github.com>
2025-09-21 21:39:24 +0700

    Add files via upload

---
+++ src/Loading.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "./Loading.css"; // tambahkan import css
+
+export default function Loading() {
+  return (
+    <div className="loading-container">
+      <img
+        src="/assets/halo.gif" // GIF loading animasi umum
+        alt="Loading"
+        className="loading-gif"
+      />
+      <span className="loading-text">
+        <span className="dot">.</span>
+        <span className="dot">.</span>
+        <span className="dot">.</span>
+      </span>
+    </div>
+  );
+}
