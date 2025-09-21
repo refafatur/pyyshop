ac5842449d43f8042fe0f69bdd50da3b26f476ee
Muhamad Refa Fatur Rahman <150717285+refafatur@users.noreply.github.com>
2025-09-21 21:40:05 +0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,41 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import Header from './Header.jsx'
-import Menu from './Menu.jsx'
-import Footer from './Footer.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import Header from "./Header.jsx";
+import Menu from "./Menu.jsx";
+import Footer from "./Footer.jsx";
+import Loading from "./Loading.jsx"; // gunakan komponen terpisah
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById("root"));
+root.render(
   <StrictMode>
-    <Header/>
-    <Menu/>
-    <App />
-    <Footer/>
-  </StrictMode>,
-)
+    <Loading />
+  </StrictMode>
+);
+
+setTimeout(() => {
+  root.render(
+    <StrictMode>
+      <>
+        <Header />
+        <Menu />
+        <App />
+        <Footer />
+      </>
+    </StrictMode>
+  );
+}, 2000);
+
+setTimeout(() => {
+  root.render(
+    <StrictMode>
+      <>
+        <Header />
+        <Menu />
+        <App />
+        <Footer />
+      </>
+    </StrictMode>
+  );
+}, 2000);
